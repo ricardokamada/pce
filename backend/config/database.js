0b82da6bb0867b855eeaa29474c9a2db20b73bb1
ricardokamada <ricardokamada@gmail.com>
2023-06-05 17:58:05 -0300

    add config .env and src app.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,16 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const Sequelize =  require('sequelize');
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  },
-};
+
+const sequelize = new Sequelize( 
+
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: 'mysql',
+      host: process.env.DB_HOST
+    });
+    
+module.exports = sequelize;
+    
+
---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+//const database = require('../config/database');
+
+const app = require('./app');
+
+app.listen(3001, ()=> {
+    console.log("App is running");
+})
